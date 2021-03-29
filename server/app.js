d5f1089c30975fffd839d70f242315979952eec3
Matthew Barton <mbarton@trinity.edu>
2021-03-29 13:08:23 -0500

    Remove Google Analystics Admin API

--- server/app.js
+++ server/app.js
@@ -1,11 +1,6 @@
-const analyticsAdmin = require('@google-analytics/admin');
-const { google } = require('@google-analytics/admin/build/protos/protos');
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
-
 
 // app.get('/', (req, res) => {
 //   res.send('hey');
@@ -17,34 +12,3 @@ app.listen(port, () => {
 })
 
 app.use(express.static('public'))
-
-async function main() {
-  // Instantiates a client using default credentials.
-  // TODO(developer): uncomment and use the following line in order to
-  //  manually set the path to the service account JSON file instead of
-  //  using the value from the GOOGLE_APPLICATION_CREDENTIALS environment
-  //  variable.
-//   const analyticsAdminClient = new analyticsAdmin.AnalyticsAdminServiceClient(
-//       {keyFilename: "C:/Users/charg/dev/matthewbarton-dev-analytics-c26ed17c44ce.json"});
- const analyticsAdminClient = new analyticsAdmin.AnalyticsAdminServiceClient({
-     keyFilename: "google-credentials.json"
- });
-
-  // Calls listAccounts() method of the Google Analytics Admin API and prints
-  // the response for each account.
-  const [accounts] = await analyticsAdminClient.listAccounts();
-
-  console.log('Accounts:');
-  accounts.forEach(account => {
-    console.log(account);
-  });
-}
-
-main(...process.argv.slice(2)).catch(err => {
-  console.error(err.message);
-  process.exitCode = 1;
-});
-process.on('unhandledRejection', err => {
-  console.error(err.message);
-  process.exitCode = 1;
-});
