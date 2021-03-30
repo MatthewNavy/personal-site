446e3aa3a329082a1d24e3f9d677e039a356c89b
Matthew Barton <mbarton@trinity.edu>
2021-03-30 15:33:11 -0500

    Last fix attempt

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -9,7 +9,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "/../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 //import bootstrap from 'bootstrap';
 
 
