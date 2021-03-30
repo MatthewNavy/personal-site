ed42c0ab2d75a5e29210e5a3bf9d4e9a2a6d9edf
Matthew Barton <mbarton@trinity.edu>
2021-03-30 00:27:16 -0500

    Remove ugly .html in URL

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-// app.get('/webapps', function(req, res) { 
-//   res.redirect('https://www.cs.trinity.edu/~mbarton/WebApps/')
-// });
+app.get('/contact', function(req, res) { 
+  res.sendFile('C:/Users/charg/dev/personal-site/public/contact.html')
+});
 
 app.listen(port, () => {
   //console.log(`matthewbarton.dev listening at http://localhost:${port}`)
