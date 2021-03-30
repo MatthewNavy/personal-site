d895ac191119e20c4987cd0c7006c004f0e49db0
Matthew Barton <mbarton@trinity.edu>
2021-03-30 14:36:23 -0500

    jQuery import?

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 //const cors = require('cors')
-//const { expr } = require('jquery');
+const { expr } = require('jquery');
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
